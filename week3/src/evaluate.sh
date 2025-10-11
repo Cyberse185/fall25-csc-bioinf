9367e16901596eb64f1779b58667081bb5ec0928
Cyberse185 <tomislavstoj22@gmail.com>
2025-10-11 13:57:26 -0700

    Update evaluate.sh

--- week3/src/evaluate.sh
+++ week3/src/evaluate.sh
@@ -5,12 +5,12 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$SCRIPT_DIR"
 
 # --- Python timing ---
-PY_MS=$(python test_phylo_python.py --just-ms | tail -n1)
+PY_MS=$(python test_phylo_python.py | tail -n1)
 
 # --- Codon timing ---
 export CODON_PYTHON=$(python -m find_libpython)
 export CODONPATH="$SCRIPT_DIR"
-CODON_MS=$(codon run test_phylo_codon.py --just-ms | tail -n1)
+CODON_MS=$(codon run test_phylo_codon.py | tail -n1)
 
 # --- Output ---
 echo "Language    Runtime"
